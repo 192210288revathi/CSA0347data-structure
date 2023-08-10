31cd7f4eb83b9ee8f8c16b5b095c719aa3393630
192210288revathi <132440459+192210288revathi@users.noreply.github.com>
2023-08-10 17:23:14 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n,square;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    square=n*n;
+    printf("%d =%d is a square root:",n,square);
+ 
+    return 0;
+}
